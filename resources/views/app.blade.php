0877f345dff6d114b0747a47428452d411681c7d
HaiHT <haiht369@gmail.com>
2016-09-11 23:28:16 +0700

    push code cuong da tich hop L5

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        @include('elements.header')
+    </head>
+    <body class="nav-md">
+        @yield('body')
+        @include('elements.footer')
+    </body>
+</html>
+
+
+
